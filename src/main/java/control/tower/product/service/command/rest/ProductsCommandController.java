044a2e3c64b985b179f766e51e9ca3055724a4bc
ajisrael <43039187+ajisrael@users.noreply.github.com>
2023-07-01 20:36:53 -0400

    f   - added open api documentation to command api

--- src/main/java/control/tower/product/service/command/rest/ProductsCommandController.java
+++ src/main/java/control/tower/product/service/command/rest/ProductsCommandController.java
@@ -5,25 +5,28 @@ import control.tower.product.service.command.commands.RemoveProductCommand;
 import control.tower.product.service.command.rest.requests.CreateProductRequestModel;
 import control.tower.product.service.command.rest.requests.RemoveProductRequestModel;
 import control.tower.product.service.command.rest.responses.ProductCreatedResponseModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/products")
+@Tag(name = "Products Command API")
 public class ProductsCommandController {
 
     @Autowired
     private CommandGateway commandGateway;
 
     @PostMapping
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create product")
     public ProductCreatedResponseModel createProduct(@Valid @RequestBody CreateProductRequestModel createProductRequestModel) {
         CreateProductCommand createProductCommand = CreateProductCommand.builder()
                 .productId(UUID.randomUUID().toString())
