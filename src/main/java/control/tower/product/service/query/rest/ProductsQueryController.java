96ce3802164c86eebe283b3e9ae16208d300b91e
ajisrael <43039187+ajisrael@users.noreply.github.com>
2023-07-01 20:43:37 -0400

    f   - aded open api documentation to query api

--- src/main/java/control/tower/product/service/query/rest/ProductsQueryController.java
+++ src/main/java/control/tower/product/service/query/rest/ProductsQueryController.java
@@ -3,29 +3,37 @@ package control.tower.product.service.query.rest;
 import control.tower.product.service.query.queries.FindAllProductsQuery;
 import control.tower.product.service.query.queries.FindProductQuery;
 import control.tower.product.service.query.querymodels.ProductQueryModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
+@Tag(name = "Products Query API")
 public class ProductsQueryController {
 
     @Autowired
     QueryGateway queryGateway;
 
     @GetMapping
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get all products")
     public List<ProductQueryModel> getProducts() {
         return queryGateway.query(new FindAllProductsQuery(),
                 ResponseTypes.multipleInstancesOf(ProductQueryModel.class)).join();
     }
 
     @GetMapping(params = "productId")
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Get product by id")
     public ProductQueryModel getProduct(String productId) {
         return queryGateway.query(new FindProductQuery(productId),
                 ResponseTypes.instanceOf(ProductQueryModel.class)).join();
